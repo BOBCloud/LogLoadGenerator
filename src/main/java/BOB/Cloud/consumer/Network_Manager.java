07696da3f7c238a28eb013ee88ab5aa2fe16e200
BOBTommy <wnsgh611@gmail.com>
2013-02-12 07:23:58 -0800

    Network Implemented

--- src/main/java/BOB/Cloud/consumer/Network_Manager.java
+++ src/main/java/BOB/Cloud/consumer/Network_Manager.java
@@ -1,13 +1,63 @@
 package BOB.Cloud.consumer;
 
-public class Network_Manager {
+import com.cloudera.flume.handlers.thrift.Priority;
+import com.cloudera.flume.handlers.thrift.ThriftFlumeEvent;
+import com.cloudera.flume.handlers.thrift.ThriftFlumeEventServer.Client;
+import java.nio.ByteBuffer;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
 
+public class Network_Manager {
+	
+	ThriftFlumeEvent tfe;
+	Map<String, java.nio.ByteBuffer> fields;
+	
 	public Network_Manager(){
-		
+		this.tfe = new ThriftFlumeEvent();
+		this.fields = new HashMap<String, ByteBuffer>();
 	}
 	
 	public void sendItem(String _item){
-		
+		tfe.fields = fields;
+        tfe.priority = Priority.INFO;
+        tfe.timestamp = new Date().getTime();
+        tfe.host = "localhost";
+
+        tfe.body = ByteBuffer.wrap(_item.getBytes());
+        Client client = getClient();
+        try {
+            client.append(tfe);
+        } catch (TException ex) {
+            Logger.getLogger(Network_Manager.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                client.close();
+            } catch (TException ex) {
+                Logger.getLogger(Network_Manager.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
 	}
+	
+	public static Client getClient() {
+        TTransport transport = new TSocket("localhost", 12345);
+        if (!transport.isOpen()) {
+            try {
+                transport.open();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+ 
+        TProtocol protocol = new TBinaryProtocol(transport);
+        return new Client(protocol);
+    }
 	
 }
