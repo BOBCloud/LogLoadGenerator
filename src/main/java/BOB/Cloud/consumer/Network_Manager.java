952c30f24bb774ed92456a74bade52c028f50b75
BOBTommy <wnsgh611@gmail.com>
2013-02-13 00:14:47 -0800

    Commenting added

--- src/main/java/BOB/Cloud/consumer/Network_Manager.java
+++ src/main/java/BOB/Cloud/consumer/Network_Manager.java
@@ -17,14 +17,35 @@ import org.apache.thrift.transport.TTransport;
 
 public class Network_Manager {
 	
+	/**
+	 * Thrift Flume Event is an event for thrift which acknowledges
+	 * it's event so, Flume's node can detect event's source
+	 * @see #tfe
+	 */
 	ThriftFlumeEvent tfe;
+	
+	/**
+	 * Map type variable filed stores a string , ByteBuffer 
+	 * for transport to the flume's node
+	 */
 	Map<String, java.nio.ByteBuffer> fields;
 	
+	/**
+	 * public Network_manager()
+	 * A constructor for network_manager
+	 * It initializes Its variable
+	 */
 	public Network_Manager(){
 		this.tfe = new ThriftFlumeEvent();
 		this.fields = new HashMap<String, ByteBuffer>();
 	}
 	
+	/**
+	 * public void sendItem(String _item)
+	 * this method used to send an chunk of string to flume node.
+	 * the parameter, _item is rebuilt suitable for flume.
+	 * @param _item a string that will be rebuilt
+	 */
 	public void sendItem(String _item){
 		tfe.fields = fields;
         tfe.priority = Priority.INFO;
@@ -48,6 +69,11 @@ public class Network_Manager {
         }
 	}
 	
+	/**
+	 * public static Client getClient()
+	 * This method is for getting Client using Thrift's class
+	 * @return an client for communicating with Flume
+	 */
 	public static Client getClient() {
         TTransport transport = new TSocket("localhost", 12345);
         if (!transport.isOpen()) {
